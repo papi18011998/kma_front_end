297d3f20aad25b9a6761458b673eecd70df199cd
papi18011998 <papaibrahima98@gmail.com>
2022-08-02 10:51:47 +0000

    integration des fonctionnalites de chaque service

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,14 +1,67 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {User} from "../model/user";
+import {environment} from "../../environments/environment";
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private token!: string|null;
+  private loggedInUsername!: null|string ;
+  private jwtHelper= new JwtHelperService();
+
 
   constructor(private http:HttpClient) { }
   public login(user:User):Observable<HttpResponse<any> | HttpErrorResponse >{
-      return this.http.post<HttpResponse<any> | HttpErrorResponse>("http://localhost:8080/users/login",user,{observe:'response'});
+      return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${environment}/users/login`,user,{observe:'response'});
+  }
+  public register(user:User):Observable<User | HttpErrorResponse >{
+    return this.http.post<User | HttpErrorResponse>(`${environment}/users/register`,user,);
+  }
+  public logout():void{
+    this.token=null;
+    this.loggedInUsername=null;
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    localStorage.removeItem('users');
+  }
+
+  public saveToken(token:string):void{
+    this.token=token;
+    localStorage.setItem('token',token);
+  }
+
+  public addUserToLocalCache(user:User):void{
+    localStorage.setItem('user',JSON.stringify(user));
+  }
+  public getUserFromLocalCache():User{
+    // @ts-ignore
+    return JSON.parse(localStorage.getItem('user'));
+  }
+  public loadToken():void{
+    // @ts-ignore
+    this.token = JSON.parse(localStorage.getItem('token'));
+  }
+  public getToken():string{
+    // @ts-ignore
+    return this.token;
+  }
+
+  public isLoggedIn():boolean|any {
+    this.loadToken()
+    if(this.token!=null && this.token !== ''){
+      if (this.jwtHelper.decodeToken(this.token).sub != null || ''){
+        if (!this.jwtHelper.isTokenExpired(this.token)){
+          this.loggedInUsername = this.jwtHelper.decodeToken(this.token).sub
+          return true
+        }
+      }
+    }else{
+      this.logout()
+      return false
+    }
   }
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../model/user";
+import {environment} from "../../environments/environment";
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private  jwtHelper = new JwtHelperService()
+  constructor(private http:HttpClient) { }
+  public getUsers():Observable<User[]| HttpErrorResponse> {
+    return this.http.get<User[]>(`${environment}/users/list`);
+  }
 
-  constructor() { }
 }
