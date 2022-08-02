199e9d4848e6030e21b2635bf1261a0f35408052
papi18011998 <papaibrahima98@gmail.com>
2022-08-02 11:19:09 +0000

    suppression d'utilisateur

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpEvent, HttpResponse} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "../model/user";
 import {environment} from "../../environments/environment";
@@ -20,5 +20,14 @@ export class UserService {
 
   public updateUser(formData: FormData):Observable<User| HttpErrorResponse> {
     return this.http.post<User>(`${environment}/users/update`,formData);
+  }
+  public resetPassword(email:string):Observable<any| HttpErrorResponse> {
+    return this.http.get(`${environment}/users/resetpassword/${email}`);
+  }
+  public updateProfileImage(formData: FormData):Observable<HttpEvent<User>| HttpErrorResponse> {
+    return this.http.post<User>(`${environment}/users/updateProfileImage`,formData,{reportProgress:true, observe:"events"});
+  }
+  public deleteUser(userId:number):Observable<any| HttpErrorResponse> {
+    return this.http.delete<any>(`${environment}/users/delete/${userId}`);
   }
 }
